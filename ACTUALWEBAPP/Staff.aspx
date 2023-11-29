bdf40c7d1d7b7d3d7e2711ee10b69c02c880bb87
Miles Koehnemann <mkoehnemann83@gmail.com>
2023-11-28 19:47:14 -0700

    added xml manipulation to STAF PAGE

--- ACTUALWEBAPP/Staff.aspx
+++ ACTUALWEBAPP/Staff.aspx
@@ -4,6 +4,26 @@
     <main aria-labelledby="title">
         <h2 id="title"><%: Title %>.</h2>
         <h3>Your application description page.</h3>
-        <p>Use this area to provide additional information.</p>
+        <p>&nbsp;</p>
+            <br />
+    <asp:Label ID="Label3" runat="server" Text="User Checks:"></asp:Label>
+    <br />
+    <asp:Label ID="Label6" runat="server" Text="Enter in a Username and search to see if it that user exists in the XML, enter a New Password and hit edit to change the password to update them to a new password or add a user that cannot add themselves"></asp:Label>
+    <br />
+    <br />
+    <asp:Label ID="Label4" runat="server" Text="UserName"></asp:Label>
+    <br />
+    <asp:TextBox ID="tbx_username" runat="server" placeholder="Username"></asp:TextBox>
+    <br />
+    <asp:Label ID="Label5" runat="server" Text="New Password"></asp:Label>
+    <br />
+    <asp:TextBox ID="tbx_password" runat="server" placeholder="Password" TextMode="Password"></asp:TextBox>
+    <br />
+    <asp:Button ID="btn_search" runat="server" OnClick="btn_search_Click" Text="Search" />
+    <asp:Button ID="btn_edit" runat="server" OnClick="btn_edit_Click" Text="Edit" />
+    <asp:Button ID="btn_addNewUser" runat="server" OnClick="btn_addNewUser_Click" Text="Add User" />    
+    <br />
+    <asp:Label ID="lbl_XML_output" runat="server" Text=""></asp:Label>
+
     </main>
 </asp:Content>
--- ACTUALWEBAPP/Staff.aspx.cs
+++ ACTUALWEBAPP/Staff.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Xml;
 
 namespace ACTUALWEBAPP
 {
@@ -11,7 +12,93 @@ namespace ACTUALWEBAPP
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            
+        }
 
+        protected void btn_search_Click(object sender, EventArgs e)
+        {
+            string usernameToSearch = tbx_username.Text;
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(Server.MapPath("Member.xml"));
+
+            XmlNodeList members = xmlDoc.SelectNodes("/Members/Member[Name='" + usernameToSearch + "']");
+
+            if (members.Count > 0)
+            {
+                foreach (XmlNode member in members)
+                {
+                    string name = member.SelectSingleNode("Name").InnerText;
+                    string password = member.SelectSingleNode("Password").InnerText;
+
+                    lbl_XML_output.Text = $"Member found: {name}, Password: {password}";
+                }
+            }
+            else
+            {
+                lbl_XML_output.Text = "Member not found.";
+            }
+        }
+
+        protected void btn_edit_Click(object sender, EventArgs e)
+        {
+            string usernameToEdit = tbx_username.Text.Trim();
+            string newPassword = tbx_password.Text.Trim();
+
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(Server.MapPath("Member.xml"));
+
+            XmlNode memberNode = xmlDoc.SelectSingleNode("/Members/Member[Name='" + usernameToEdit + "']");
+
+            if (memberNode != null)
+            {
+                XmlNode passwordNode = memberNode.SelectSingleNode("Password");
+                passwordNode.InnerText = newPassword;
+
+                xmlDoc.Save(Server.MapPath("Member.xml"));
+                lbl_XML_output.Text = "Password updated successfully.";
+            }
+            else
+            {
+                lbl_XML_output.Text = "Member not found.";
+            }
+        }
+
+        protected void btn_addNewUser_Click(object sender, EventArgs e)
+        {
+            string newUsername = tbx_username.Text.Trim();
+            string newPassword = tbx_password.Text.Trim();
+
+            // Load XML document
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(Server.MapPath("Member.xml"));
+
+            // Check if the username already exists
+            XmlNode existingMember = xmlDoc.SelectSingleNode("/Members/Member[Name='" + newUsername + "']");
+            if (existingMember != null)
+            {
+                lbl_XML_output.Text = "Error: User already exists.";
+            }
+            else
+            {
+                // Create a new member node
+                XmlNode newMember = xmlDoc.CreateElement("Member");
+                XmlNode nameNode = xmlDoc.CreateElement("Name");
+                nameNode.InnerText = newUsername;
+                XmlNode passwordNode = xmlDoc.CreateElement("Password");
+                passwordNode.InnerText = newPassword;
+
+                // Append name and password nodes to the new member node
+                newMember.AppendChild(nameNode);
+                newMember.AppendChild(passwordNode);
+
+                // Append the new member node to the Members node
+                xmlDoc.SelectSingleNode("/Members").AppendChild(newMember);
+
+                // Save the changes to the XML file
+                xmlDoc.Save(Server.MapPath("Member.xml"));
+
+                lbl_XML_output.Text = "Member added successfully.";
+            }
         }
     }
 }
